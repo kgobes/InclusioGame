d427dec433aa5a10bab6a8baf987050e8e9c5d56
Kira Gobes <kira.gobes@gmail.com>
2016-06-28 09:23:26 -0400

    Character picture changes

--- Assets/textChange.cs
+++ Assets/textChange.cs
@@ -5,14 +5,27 @@ using System.Collections;
 public class textChange : MonoBehaviour {
 	public static Text charName;
 	public Button genChar;
+	public Image charPic;
+	public Sprite fish;
+	public Sprite lion;
+	public Sprite penguin;
 	// Use this for initialization
 	void Start () {
 		charName = GameObject.Find ("charName").GetComponent<Text>();
 		charName.enabled = false;
 		genChar = GameObject.Find ("GenChar").GetComponent<Button>();
 		genChar.GetComponentInChildren<Text>().text = "Generate Character";
-		//connect start text to button to change scene ***
+		charPic = GameObject.Find ("charPic").GetComponent<Image>();
+		//charPic = GetComponent<SpriteRenderer> ();
+		charPic.enabled = false;
 
+		//images
+		/*fish = GameObject.Find ("fish").GetComponent<Sprite>();
+		lion = GameObject.Find ("lion").GetComponent<Sprite>();
+		penguin = GameObject.Find ("penguin").GetComponent<Sprite>();*/
+		fish = Resources.Load <Sprite>("fish");
+		lion= Resources.Load <Sprite>("lion");
+		penguin= Resources.Load <Sprite>("peng");
 	}
 	
 	// Update is called once per frame
@@ -25,15 +38,35 @@ public class textChange : MonoBehaviour {
 	}
 
 	public string generateCharacter(){
+		charPic.enabled = true;
 		int x = Random.Range (1,4);
 		Debug.Log (x);
 		string character = "";
-		if (x == 1)
-			character = "Turtle";
-		else if(x == 2)
+		if (x == 1) {
+			character = "Fish";
+			//charPic.sprite = Resources.Load<Sprite>("fish.jpg");
+			//charPic.GetComponent<Image>().sprite = fish;
+			if(fish)
+				charPic.sprite = fish;
+			else
+				Debug.LogError ("Sprite not found", this);
+		} else if (x == 2) {
 			character = "Lion";
-		else if(x == 3)
-			character = "Bird";
+			//charPic.sprite = Resources.Load<Sprite>("lion.jpg");
+			//charPic.GetComponent<Image>().sprite = lion;
+			if(lion)
+				charPic.sprite = lion;
+			else
+				Debug.LogError ("Sprite not found", this);
+		} else if (x == 3) {
+			character = "Penguin";
+			//charPic.sprite = Resources.Load<Sprite>("peng.png");
+			//charPic.GetComponent<Image>().sprite = penguin;
+			if(penguin)
+				charPic.sprite = penguin;
+			else
+				Debug.LogError ("Sprite not found", this);
+		}
 		return character;
 
 	}
