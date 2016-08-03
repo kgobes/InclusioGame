828b9ec030772bece61b922fbf7067e343c9ec25
Chris <christopher.johnso01@mymail.champlain.edu>
2016-08-03 12:02:45 -0400

    +a bunch of missing files from last commit. Sorry!
    -Chris

---
+++ Assets/Scripts/DisplaySplashScreen.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class DisplaySplashScreen : MonoBehaviour
+{
+    public Image logo1;
+    public Image logo2;
+
+    public float fadeDurationSeconds = 1f;
+    public float timeBetweenLogosSeconds = 1f;
+    public float displayDurationSeconds = 2f;
+
+	// Use this for initialization
+	void Start ()
+    {
+        logo1.canvasRenderer.SetAlpha(0f);
+        logo2.canvasRenderer.SetAlpha(0f);
+        StartCoroutine(DisplayLogo1());
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        
+	}
+
+    IEnumerator DisplayLogo1()
+    {
+        yield return new WaitForSeconds(timeBetweenLogosSeconds);
+
+        logo1.CrossFadeAlpha(1f, fadeDurationSeconds, false);
+
+        yield return new WaitForSeconds(displayDurationSeconds);
+
+        logo1.CrossFadeAlpha(0f, fadeDurationSeconds, false);
+
+        StartCoroutine(DisplayLogo2());
+        StopCoroutine(DisplayLogo1());
+    }
+
+    IEnumerator DisplayLogo2()
+    {
+        yield return new WaitForSeconds(timeBetweenLogosSeconds);
+
+        logo2.CrossFadeAlpha(1f, fadeDurationSeconds, false);
+
+        yield return new WaitForSeconds(displayDurationSeconds);
+
+        logo2.CrossFadeAlpha(0f, fadeDurationSeconds, false);
+
+        yield return new WaitForSeconds(timeBetweenLogosSeconds);
+
+        StopCoroutine(DisplayLogo2());
+
+        Application.LoadLevel("StartScreen");
+    }
+}
---
+++ Assets/Scripts/StoryIntroController.cs
@@ -0,0 +1,110 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class StoryIntroController : MonoBehaviour
+{
+    public Text textHeader;
+    public Text textFooter;
+    public Text textBody1;
+    public Text textBody2;
+
+	// Use this for initialization
+	void Start ()
+    {
+        textHeader.canvasRenderer.SetAlpha(0f);
+        textFooter.canvasRenderer.SetAlpha(0f);
+        textBody1.canvasRenderer.SetAlpha(0f);
+        textBody2.canvasRenderer.SetAlpha(0f);
+
+        StartCoroutine(DisplayStoryText());
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    IEnumerator DisplayStoryText()
+    {
+        yield return new WaitForSeconds(2f);
+
+        textHeader.CrossFadeAlpha(1f, 2f, false);
+
+        yield return new WaitForSeconds(2f);
+
+        textBody1.CrossFadeAlpha(1f, 2f, false);
+
+        yield return new WaitForSeconds(3f);
+
+        textBody2.CrossFadeAlpha(1f, 2f, false);
+
+        yield return new WaitForSeconds(6f);
+
+        textHeader.CrossFadeAlpha(0f, 2f, false);
+        textBody1.CrossFadeAlpha(0f, 2f, false);
+        textBody2.CrossFadeAlpha(0f, 2f, false);
+
+        yield return new WaitForSeconds(3f);
+
+        textBody1.text = "Your mission is to get Elf back through the maze.";
+        textBody2.text = "Along the way, you'll face doors you can't fit through, characters who won't let you pass, traps set by others, and diminishing health.";
+
+        textBody1.CrossFadeAlpha(1f, 2f, false);
+
+        yield return new WaitForSeconds(4f);
+
+        textBody2.CrossFadeAlpha(1f, 2f, false);
+
+        yield return new WaitForSeconds(7f);
+
+        textBody1.CrossFadeAlpha(0f, 2f, false);
+        textBody2.CrossFadeAlpha(0f, 2f, false);
+
+        yield return new WaitForSeconds(3f);
+
+        textBody1.text = "You'll have the help of other characters that you befriend throughout the game.";
+        textBody2.text = "But take caution...Some might be a friendly face hiding evil, and others might be a friend wearing an evil face.";
+
+        textBody1.CrossFadeAlpha(1f, 2f, false);
+
+        yield return new WaitForSeconds(3f);
+
+        textBody2.CrossFadeAlpha(1f, 2f, false);
+
+        yield return new WaitForSeconds(6f);
+
+        textBody1.CrossFadeAlpha(0f, 2f, false);
+        textBody2.CrossFadeAlpha(0f, 2f, false);
+
+        yield return new WaitForSeconds(3f);
+
+        textBody1.text = "Elf needs you to see past stereotypes and think critically to get back into place.";
+        textBody2.text = "The fate of Elf, and all the characters of the book, rests with you.";
+
+        textBody1.CrossFadeAlpha(1f, 2f, false);
+
+        yield return new WaitForSeconds(6f);
+
+        textBody2.CrossFadeAlpha(1f, 2f, false);
+
+        yield return new WaitForSeconds(5f);
+
+        textFooter.CrossFadeAlpha(1f, 2f, false);
+
+        yield return new WaitForSeconds(2f);
+
+        textBody1.CrossFadeAlpha(0f, 2f, false);
+        textBody2.CrossFadeAlpha(0f, 2f, false);
+
+        yield return new WaitForSeconds(3f);
+
+        textFooter.CrossFadeAlpha(0f, 2f, false);
+
+        yield return new WaitForSeconds(3f);
+
+        StopCoroutine(DisplayStoryText());
+        Application.LoadLevel("MazeScene");
+    }
+}
