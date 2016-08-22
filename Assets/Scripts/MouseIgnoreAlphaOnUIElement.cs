c067ee7044a751c6388fe4a2affd9a6ef5612c41
Chris <christopher.johnso01@mymail.champlain.edu>
2016-08-22 12:46:19 -0400

    *2nd attempt to fix problems caused by wrong version
    -Chris

---
+++ Assets/Scripts/MouseIgnoreAlphaOnUIElement.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MouseIgnoreAlphaOnUIElement : MonoBehaviour
+{
+    Image button;
+
+    // Use this for initialization
+    void Start()
+    {
+        
+    }
+}
