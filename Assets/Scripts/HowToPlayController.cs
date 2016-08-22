77b8cee1eb8db8ab4c4f73541f61001425d49ead
Chris <christopher.johnso01@mymail.champlain.edu>
2016-08-22 17:27:39 -0400

    +began work on HowToPlayScreen.unity
    -Chris

---
+++ Assets/Scripts/HowToPlayController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HowToPlayController : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void OnPressPlay()
+    {
+        Application.LoadLevel(4);
+    }
+}
