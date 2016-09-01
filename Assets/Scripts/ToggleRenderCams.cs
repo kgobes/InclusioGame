7e315b9a4af80284b8fda7648343f5798d247608
Chris <christopher.johnso01@mymail.champlain.edu>
2016-09-01 12:13:41 -0400

    *fixed problems of last commit +various optimization
    -Chris

---
+++ Assets/Scripts/ToggleRenderCams.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ToggleRenderCams : MonoBehaviour
+{
+    public GameObject camContainer;
+
+	void Update ()
+    {
+       camContainer.SetActive(GetComponent<CanvasGroup>().alpha > 0.1f);
+	}
+}
