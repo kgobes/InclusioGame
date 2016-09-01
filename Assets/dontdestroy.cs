8c8ecebb6c76d456f8c351b212841cc2acbf2fa6
Chris <christopher.johnso01@mymail.champlain.edu>
2016-09-01 12:37:38 -0400

    *now I think it's finally fixed
    -Chris

---
+++ Assets/dontdestroy.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class dontdestroy : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+}
